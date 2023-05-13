601631a6a5c50aff9ca042fdf2916bf4817928f5
btr13010 <tranquocbao290402@gmail.com>
2023-05-14 03:39:55 +0700

    Update experiments

--- experiments.py
+++ experiments.py
@@ -1,12 +1,10 @@
-# import os
-# os.environ['SDL_VIDEODRIVER']='dummy'
-
 import gymnasium as gym
-# env = gym.make("LunarLander-v2", render_mode="human")
-env = gym.make("Hopper-v2", render_mode="human")
+
+env = gym.make("LunarLander-v2", render_mode="human")
+# env = gym.make("Hopper-v2", render_mode="human")
 # env = gym.make('CartPole-v1', render_mode="human")
 observation, info = env.reset()
-    
+
 for i in range(1000):
     action = env.action_space.sample()  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
