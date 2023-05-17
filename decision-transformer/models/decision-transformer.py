aceb232fdcf30b9ebf0b94a5d920f6c8eec70d2c
btr13010 <tranquocbao290402@gmail.com>
2023-05-17 16:26:56 +0700

    Update dt model

--- decision-transformer/models/decision-transformer.py
+++ decision-transformer/models/decision-transformer.py
@@ -5,374 +5,147 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-logger = logging.getLogger(__name__)
-
 import numpy as np
 
-class GELU(nn.Module):
-    def forward(self, input):
-        return F.gelu(input)
-
-
-class GPTConfig:
-    """ base GPT config, params common to all GPT versions """
-
-    embd_pdrop = 0.1
-    resid_pdrop = 0.1
-    attn_pdrop = 0.1
-
-    def __init__(self, vocab_size, block_size, **kwargs):
-        self.vocab_size = vocab_size
-        self.block_size = block_size
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-
-
-class GPT1Config(GPTConfig):
-    """ GPT-1 like network roughly 125M params """
-
-    n_layer = 12
-    n_head = 12
-    n_embd = 768
-
-    # n_layer = 8
-    # n_head = 8
-    # n_embd = 64
-
-class CausalSelfAttention(nn.Module):
-    """
-    A vanilla multi-head masked self-attention layer with a projection at the end.
-    It is possible to use torch.nn.MultiheadAttention here but I am including an
-    explicit implementation here to show that there is nothing too scary here.
-    """
-
-    def __init__(self, config):
+class MaskedCausalAttention(nn.Module):
+    def __init__(self, h_dim, max_T, n_heads, drop_p):
         super().__init__()
-        assert config.n_embd % config.n_head == 0
-        # key, query, value projections for all heads
-        self.key = nn.Linear(config.n_embd, config.n_embd)
-        self.query = nn.Linear(config.n_embd, config.n_embd)
-        self.value = nn.Linear(config.n_embd, config.n_embd)
-        # regularization
-        self.attn_drop = nn.Dropout(config.attn_pdrop)
-        self.resid_drop = nn.Dropout(config.resid_pdrop)
-        # output projection
-        self.proj = nn.Linear(config.n_embd, config.n_embd)
-        # causal mask to ensure that attention is only applied to the left in the input sequence
-        # self.register_buffer("mask", torch.tril(torch.ones(config.block_size, config.block_size))
-        #                              .view(1, 1, config.block_size, config.block_size))
-        self.register_buffer(
-            "mask",
-            torch.tril(torch.ones(config.block_size + 1, config.block_size + 1)).view(
-                1, 1, config.block_size + 1, config.block_size + 1
-            ),
-        )
-        self.n_head = config.n_head
 
-    def forward(self, x, layer_past=None):
-        B, T, C = x.size()
+        self.n_heads = n_heads
+        self.max_T = max_T # vocab_size
 
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        k = (
-            self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        )  # (B, nh, T, hs)
-        q = (
-            self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        )  # (B, nh, T, hs)
-        v = (
-            self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        )  # (B, nh, T, hs)
+        self.q_net = nn.Linear(h_dim, h_dim)
+        self.k_net = nn.Linear(h_dim, h_dim)
+        self.v_net = nn.Linear(h_dim, h_dim)
 
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.mask[:, :, :T, :T] == 0, float("-inf"))
-        att = F.softmax(att, dim=-1)
-        att = self.attn_drop(att)
-        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = (
-            y.transpose(1, 2).contiguous().view(B, T, C)
-        )  # re-assemble all head outputs side by side
+        self.proj_net = nn.Linear(h_dim, h_dim)
 
-        # output projection
-        y = self.resid_drop(self.proj(y))
-        return y
+        self.att_drop = nn.Dropout(drop_p)
+        self.proj_drop = nn.Dropout(drop_p)
+
+        ones = torch.ones((max_T, max_T))
+        mask = torch.tril(ones).view(1, 1, max_T, max_T) # mask for masked attention
+
+        # register buffer makes sure mask does not get updated
+        # during backpropagation
+        self.register_buffer('mask',mask)
+
+    def forward(self, x):
+        B, T, C = x.shape # batch size, seq length, h_dim * n_heads
+
+        N, D = self.n_heads, C // self.n_heads # N = num heads, D = attention dim
+
+        # rearrange q, k, v as (B, N, T, D)
+        q = self.q_net(x).view(B, T, N, D).transpose(1,2)
+        k = self.k_net(x).view(B, T, N, D).transpose(1,2)
+        v = self.v_net(x).view(B, T, N, D).transpose(1,2)
+
+        # weights (B, N, T, T)
+        weights = q @ k.transpose(2,3) / math.sqrt(D)
+        # causal mask applied to weights
+        weights = weights.masked_fill(self.mask[...,:T,:T] == 0, float('-inf'))
+        # normalize weights, all -inf -> 0 after softmax
+        normalized_weights = F.softmax(weights, dim=-1)
+
+        # attention (B, N, T, D)
+        attention = self.att_drop(normalized_weights @ v)
+
+        # gather heads and project (B, N, T, D) -> (B, T, N*D)
+        attention = attention.transpose(1, 2).contiguous().view(B,T,N*D)
+
+        out = self.proj_drop(self.proj_net(attention))
+        return out
 
 
 class Block(nn.Module):
-    """ an unassuming Transformer block """
-
-    def __init__(self, config):
+    def __init__(self, h_dim, max_T, n_heads, drop_p):
         super().__init__()
-        self.ln1 = nn.LayerNorm(config.n_embd)
-        self.ln2 = nn.LayerNorm(config.n_embd)
-        self.attn = CausalSelfAttention(config)
+        self.attention = MaskedCausalAttention(h_dim, max_T, n_heads, drop_p)
         self.mlp = nn.Sequential(
-            nn.Linear(config.n_embd, 4 * config.n_embd),
-            GELU(),
-            nn.Linear(4 * config.n_embd, config.n_embd),
-            nn.Dropout(config.resid_pdrop),
-        )
+                nn.Linear(h_dim, 4*h_dim),
+                nn.GELU(),
+                nn.Linear(4*h_dim, h_dim),
+                nn.Dropout(drop_p),
+            )
+        self.ln1 = nn.LayerNorm(h_dim)
+        self.ln2 = nn.LayerNorm(h_dim)
 
     def forward(self, x):
-        x = x + self.attn(self.ln1(x))
-        x = x + self.mlp(self.ln2(x))
+        # Attention -> LayerNorm -> MLP -> LayerNorm
+        x = x + self.attention(x) # residual
+        x = self.ln1(x)
+        x = x + self.mlp(x) # residual
+        x = self.ln2(x)
         return x
 
 
-class GPT(nn.Module):
-    """  the full GPT language model, with a context size of block_size """
-
-    def __init__(self, config):
+class DecisionTransformer(nn.Module):
+    def __init__(self, state_dim, act_dim, n_blocks, h_dim, context_len, 
+                 n_heads, drop_p, max_timestep=4096):
         super().__init__()
 
-        self.config = config
+        self.state_dim = state_dim
+        self.act_dim = act_dim
+        self.h_dim = h_dim
 
-        self.model_type = config.model_type
+        ### transformer blocks
+        input_seq_len = 3 * context_len
+        blocks = [Block(h_dim, input_seq_len, n_heads, drop_p) for _ in range(n_blocks)]
+        self.transformer = nn.Sequential(*blocks)
 
-        # input embedding stem
-        self.tok_emb = nn.Embedding(config.vocab_size, config.n_embd)
-        # self.pos_emb = nn.Parameter(torch.zeros(1, config.block_size, config.n_embd))
-        self.pos_emb = nn.Parameter(
-            torch.zeros(1, config.block_size + 1, config.n_embd)
-        )
-        self.global_pos_emb = nn.Parameter(
-            torch.zeros(1, config.max_timestep + 1, config.n_embd)
-        )
-        self.drop = nn.Dropout(config.embd_pdrop)
+        ### projection heads (project to embedding)
+        self.embed_ln = nn.LayerNorm(h_dim)
+        self.embed_timestep = nn.Embedding(max_timestep, h_dim)
+        self.embed_rtg = torch.nn.Linear(1, h_dim)
+        self.embed_state = torch.nn.Linear(state_dim, h_dim)
+        
+        # discrete actions
+        self.embed_action = torch.nn.Embedding(act_dim, h_dim)
+        use_action_tanh = False # False for discrete actions
 
-        # transformer
-        self.blocks = nn.Sequential(*[Block(config) for _ in range(config.n_layer)])
-        # decoder head
-        self.ln_f = nn.LayerNorm(config.n_embd)
-        self.head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
-
-        self.block_size = config.block_size
-        self.apply(self._init_weights)
-
-        logger.info(
-            "number of parameters: %e", sum(p.numel() for p in self.parameters())
-        )
-
-        self.state_encoder = nn.Sequential(
-            nn.Conv2d(4, 32, 8, stride=4, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 4, stride=2, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3, stride=1, padding=0),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(3136, config.n_embd),
-            nn.Tanh(),
+        # # continuous actions
+        # self.embed_action = torch.nn.Linear(act_dim, h_dim)
+        # use_action_tanh = True # True for continuous actions
+        
+        ### prediction heads
+        self.predict_rtg = torch.nn.Linear(h_dim, 1)
+        self.predict_state = torch.nn.Linear(h_dim, state_dim)
+        self.predict_action = nn.Sequential(
+            *([nn.Linear(h_dim, act_dim)] + ([nn.Tanh()] if use_action_tanh else []))
         )
 
-        self.ret_emb = nn.Sequential(nn.Linear(1, config.n_embd), nn.Tanh())
 
-        self.action_embeddings = nn.Sequential(
-            nn.Embedding(config.vocab_size, config.n_embd), nn.Tanh()
-        )
-        nn.init.normal_(self.action_embeddings[0].weight, mean=0.0, std=0.02)
+    def forward(self, timesteps, states, actions, returns_to_go):
 
-    def get_block_size(self):
-        return self.block_size
+        B, T, _ = states.shape
 
-    def _init_weights(self, module):
-        if isinstance(module, (nn.Linear, nn.Embedding)):
-            module.weight.data.normal_(mean=0.0, std=0.02)
-            if isinstance(module, nn.Linear) and module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
+        time_embeddings = self.embed_timestep(timesteps)
 
-    def configure_optimizers(self, train_config):
-        """
-        This long function is unfortunately doing something very simple and is being very defensive:
-        We are separating out all parameters of the model into two buckets: those that will experience
-        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
-        We are then returning the PyTorch optimizer object.
-        """
+        # time embeddings are treated similar to positional embeddings
+        state_embeddings = self.embed_state(states) + time_embeddings
+        action_embeddings = self.embed_action(actions) + time_embeddings
+        returns_embeddings = self.embed_rtg(returns_to_go) + time_embeddings
 
-        # separate out all parameters to those that will and won't experience regularizing weight decay
-        decay = set()
-        no_decay = set()
-        # whitelist_weight_modules = (torch.nn.Linear, )
-        whitelist_weight_modules = (torch.nn.Linear, torch.nn.Conv2d)
-        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
-        for mn, m in self.named_modules():
-            for pn, p in m.named_parameters():
-                fpn = "%s.%s" % (mn, pn) if mn else pn  # full param name
+        # stack rtg, states and actions and reshape sequence as
+        # (r1, s1, a1, r2, s2, a2 ...)
+        h = torch.stack(
+            (returns_embeddings, state_embeddings, action_embeddings), dim=1
+        ).permute(0, 2, 1, 3).reshape(B, 3 * T, self.h_dim)
 
-                if pn.endswith("bias"):
-                    # all biases will not be decayed
-                    no_decay.add(fpn)
-                elif pn.endswith("weight") and isinstance(m, whitelist_weight_modules):
-                    # weights of whitelist modules will be weight decayed
-                    decay.add(fpn)
-                elif pn.endswith("weight") and isinstance(m, blacklist_weight_modules):
-                    # weights of blacklist modules will NOT be weight decayed
-                    no_decay.add(fpn)
+        h = self.embed_ln(h)
+        
+        # transformer and prediction
+        h = self.transformer(h)
 
-        # special case the position embedding parameter in the root GPT module as not decayed
-        no_decay.add("pos_emb")
-        no_decay.add("global_pos_emb")
+        # get h reshaped such that its size = (B x 3 x T x h_dim) and
+        # h[:, 0, t] is conditioned on r_0, s_0, a_0 ... r_t
+        # h[:, 1, t] is conditioned on r_0, s_0, a_0 ... r_t, s_t
+        # h[:, 2, t] is conditioned on r_0, s_0, a_0 ... r_t, s_t, a_t
+        h = h.reshape(B, T, 3, self.h_dim).permute(0, 2, 1, 3)
 
-        # validate that we considered every parameter
-        param_dict = {pn: p for pn, p in self.named_parameters()}
-        inter_params = decay & no_decay
-        union_params = decay | no_decay
-        assert (
-            len(inter_params) == 0
-        ), "parameters %s made it into both decay/no_decay sets!" % (str(inter_params),)
-        assert len(param_dict.keys() - union_params) == 0, (
-            "parameters %s were not separated into either decay/no_decay set!"
-            % (str(param_dict.keys() - union_params),)
-        )
-
-        # create the pytorch optimizer object
-        optim_groups = [
-            {
-                "params": [param_dict[pn] for pn in sorted(list(decay))],
-                "weight_decay": train_config.weight_decay,
-            },
-            {
-                "params": [param_dict[pn] for pn in sorted(list(no_decay))],
-                "weight_decay": 0.0,
-            },
-        ]
-        optimizer = torch.optim.AdamW(
-            optim_groups, lr=train_config.learning_rate, betas=train_config.betas
-        )
-        return optimizer
-
-    # state, action, and return
-    def forward(self, states, actions, targets=None, rtgs=None, timesteps=None):
-        # states: (batch, block_size, 4*84*84)
-        # actions: (batch, block_size, 1)
-        # targets: (batch, block_size, 1)
-        # rtgs: (batch, block_size, 1)
-        # timesteps: (batch, 1, 1)
-
-        state_embeddings = self.state_encoder(
-            states.reshape(-1, 4, 84, 84).type(torch.float32).contiguous()
-        )  # (batch * block_size, n_embd)
-        state_embeddings = state_embeddings.reshape(
-            states.shape[0], states.shape[1], self.config.n_embd
-        )  # (batch, block_size, n_embd)
-
-        if actions is not None and self.model_type == "reward_conditioned":
-            rtg_embeddings = self.ret_emb(rtgs.type(torch.float32))
-            action_embeddings = self.action_embeddings(
-                actions.type(torch.long).squeeze(-1)
-            )  # (batch, block_size, n_embd)
-
-            token_embeddings = torch.zeros(
-                (
-                    states.shape[0],
-                    states.shape[1] * 3 - int(targets is None),
-                    self.config.n_embd,
-                ),
-                dtype=torch.float32,
-                device=state_embeddings.device,
-            )
-            token_embeddings[:, ::3, :] = rtg_embeddings
-            token_embeddings[:, 1::3, :] = state_embeddings
-            token_embeddings[:, 2::3, :] = action_embeddings[
-                :, -states.shape[1] + int(targets is None) :, :
-            ]
-        elif (
-            actions is None and self.model_type == "reward_conditioned"
-        ):  # only happens at very first timestep of evaluation
-            rtg_embeddings = self.ret_emb(rtgs.type(torch.float32))
-
-            token_embeddings = torch.zeros(
-                (states.shape[0], states.shape[1] * 2, self.config.n_embd),
-                dtype=torch.float32,
-                device=state_embeddings.device,
-            )
-            token_embeddings[:, ::2, :] = rtg_embeddings  # really just [:,0,:]
-            token_embeddings[:, 1::2, :] = state_embeddings  # really just [:,1,:]
-        elif actions is not None and self.model_type == "naive":
-            action_embeddings = self.action_embeddings(
-                actions.type(torch.long).squeeze(-1)
-            )  # (batch, block_size, n_embd)
-
-            token_embeddings = torch.zeros(
-                (
-                    states.shape[0],
-                    states.shape[1] * 2 - int(targets is None),
-                    self.config.n_embd,
-                ),
-                dtype=torch.float32,
-                device=state_embeddings.device,
-            )
-            token_embeddings[:, ::2, :] = state_embeddings
-            token_embeddings[:, 1::2, :] = action_embeddings[
-                :, -states.shape[1] + int(targets is None) :, :
-            ]
-        elif (
-            actions is None and self.model_type == "naive"
-        ):  # only happens at very first timestep of evaluation
-            token_embeddings = state_embeddings
-        else:
-            raise NotImplementedError()
-
-        batch_size = states.shape[0]
-        all_global_pos_emb = torch.repeat_interleave(
-            self.global_pos_emb, batch_size, dim=0
-        )  # batch_size, traj_length, n_embd
-
-        position_embeddings = (
-            torch.gather(
-                all_global_pos_emb,
-                1,
-                torch.repeat_interleave(timesteps, self.config.n_embd, dim=-1),
-            )
-            + self.pos_emb[:, : token_embeddings.shape[1], :]
-        )
-
-        x = self.drop(token_embeddings + position_embeddings)
-        x = self.blocks(x)
-        x = self.ln_f(x)
-        logits = self.head(x)
-
-        if actions is not None and self.model_type == "reward_conditioned":
-            logits = logits[:, 1::3, :]  # only keep predictions from state_embeddings
-        elif actions is None and self.model_type == "reward_conditioned":
-            logits = logits[:, 1:, :]
-        elif actions is not None and self.model_type == "naive":
-            logits = logits[:, ::2, :]  # only keep predictions from state_embeddings
-        elif actions is None and self.model_type == "naive":
-            logits = logits  # for completeness
-        else:
-            raise NotImplementedError()
-
-        # if we are given some desired targets also calculate the loss
-        loss = None
-        if targets is not None:
-            loss = F.cross_entropy(
-                logits.reshape(-1, logits.size(-1)), targets.reshape(-1)
-            )
-
-        return logits, loss
-
-
-if __name__ == "__main__":
-    vocab_size = 4
-    block_size = 90
-    model_type = "reward_conditioned"
-    timesteps = 2654
-
-    mconf = GPTConfig(
-        vocab_size,
-        block_size,
-        n_layer=6,
-        n_head=8,
-        n_embd=128,
-        model_type=model_type,
-        max_timestep=timesteps,
-    )
-    model = GPT(mconf)
-
-    checkpoint_path = "checkpoints/Breakout_123.pth"  # or Pong, Qbert, Seaquest
-    checkpoint = torch.load(checkpoint_path)
-    model.load_state_dict(checkpoint)
+        # get predictions
+        return_preds = self.predict_rtg(h[:,2])     # predict next rtg given r, s, a
+        state_preds = self.predict_state(h[:,2])    # predict next state given r, s, a
+        action_preds = self.predict_action(h[:,1])  # predict action given r, s
+    
+        return state_preds, action_preds, return_preds
---
+++ decision-transformer/models/dt-trainer.py
@@ -0,0 +1,187 @@
+from datetime import datetime
+import csv
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import gym
+import os
+from utils import D4RLTrajectoryDataset, GPTConfig
+from decision_transformer import DecisionTransformer
+
+# --------------------------------
+# Configuration
+# --------------------------------
+
+device = 'cpu'
+
+dataset = "expert"
+rtg_scale = 1000
+
+env_name = 'Breakout-v0'
+rtg_target = 5000
+env_d4rl_name = f'breakout-{dataset}-v2'
+
+# load data from this file
+dataset_path = f'data/{env_d4rl_name}.pkl'
+
+# saves model and csv in this directory
+log_dir = "./dt_runs/"
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+# --------------------------------
+# Training
+# --------------------------------
+
+start_time = datetime.now().replace(microsecond=0)
+
+start_time_str = start_time.strftime("%y-%m-%d-%H-%M-%S")
+
+prefix = "dt_" + env_d4rl_name
+
+save_model_name =  prefix + "_model_" + start_time_str + ".pt"
+save_model_path = os.path.join(log_dir, save_model_name)
+save_best_model_path = save_model_path[:-3] + "_best.pt"
+
+log_csv_name = prefix + "_log_" + start_time_str + ".csv"
+log_csv_path = os.path.join(log_dir, log_csv_name)
+
+
+csv_writer = csv.writer(open(log_csv_path, 'a', 1))
+csv_header = (["duration", "num_updates", "action_loss", 
+               "eval_avg_reward", "eval_avg_ep_len", "eval_d4rl_score"])
+
+csv_writer.writerow(csv_header)
+
+
+print("=" * 60)
+print("start time: " + start_time_str)
+print("=" * 60)
+
+print("device set to: " + str(device))
+print("dataset path: " + dataset_path)
+print("model save path: " + save_model_path)
+print("log csv save path: " + log_csv_path)
+
+env = gym.make(env_name)
+
+state_dim = env.observation_space.shape
+act_dim = env.action_space.n
+
+conf = GPTConfig(state_dim=state_dim, act_dim=act_dim)
+
+traj_dataset = D4RLTrajectoryDataset(dataset_path, conf.context_len, rtg_scale)
+
+traj_data_loader = DataLoader(traj_dataset,
+						batch_size=conf.batch_size,
+						shuffle=True,
+						pin_memory=True,
+						drop_last=True) 
+
+data_iter = iter(traj_data_loader)
+
+## get state stats from dataset
+# state_mean, state_std = traj_dataset.get_state_stats()
+
+model = DecisionTransformer(conf).to(device)
+  
+optimizer = torch.optim.AdamW(
+					model.parameters(), 
+					lr=lr, 
+					weight_decay=conf.wt_decay
+				)
+
+scheduler = torch.optim.lr_scheduler.LambdaLR(
+		optimizer,
+		lambda steps: min((steps+1)/conf.warmup_steps, 1)
+	)
+
+max_d4rl_score = -1.0
+total_updates = 0
+
+for i_train_iter in range(conf.max_train_iters):
+
+	log_action_losses = []	
+	model.train()
+ 
+	for _ in range(conf.num_updates_per_iter):
+		try:
+			timesteps, states, actions, returns_to_go, traj_mask = next(data_iter)
+		except StopIteration:
+			data_iter = iter(traj_data_loader)
+			timesteps, states, actions, returns_to_go, traj_mask = next(data_iter)
+
+		timesteps = timesteps.to(device)	# B x T
+		states = states.to(device)			# B x T x state_dim
+		actions = actions.to(device)		# B x T x act_dim
+		returns_to_go = returns_to_go.to(device).unsqueeze(dim=-1) # B x T x 1
+		traj_mask = traj_mask.to(device)	# B x T
+
+		action_target = torch.clone(actions).detach().to(device)
+	
+		state_preds, action_preds, return_preds = model.forward(
+														timesteps=timesteps,
+														states=states,
+														actions=actions,
+														returns_to_go=returns_to_go
+													)
+
+		# only consider non padded elements
+		action_preds = action_preds.view(-1, act_dim)[traj_mask.view(-1,) > 0]
+		action_target = action_target.view(-1, act_dim)[traj_mask.view(-1,) > 0]
+
+		action_loss = F.mse_loss(action_preds, action_target, reduction='mean')
+
+		optimizer.zero_grad()
+		action_loss.backward()
+		torch.nn.utils.clip_grad_norm_(model.parameters(), 0.25)
+		optimizer.step()
+		scheduler.step()
+
+		log_action_losses.append(action_loss.detach().cpu().item())
+
+
+	mean_action_loss = np.mean(log_action_losses)
+	time_elapsed = str(datetime.now().replace(microsecond=0) - start_time)
+
+	total_updates += conf.num_updates_per_iter
+
+	log_str = ("=" * 60 + '\n' +
+			"time elapsed: " + time_elapsed  + '\n' +
+			"num of updates: " + str(total_updates) + '\n' +
+			"action loss: " +  format(mean_action_loss, ".5f")
+			)
+
+	print(log_str)
+
+	log_data = [time_elapsed, total_updates, mean_action_loss]
+
+	csv_writer.writerow(log_data)
+	
+	# save model
+	# print("max d4rl score: " + format(max_d4rl_score, ".5f"))
+	# if eval_d4rl_score >= max_d4rl_score:
+	# 	print("saving max d4rl score model at: " + save_best_model_path)
+	# 	torch.save(model.state_dict(), save_best_model_path)
+	# 	max_d4rl_score = eval_d4rl_score
+
+	print("saving current model at: " + save_model_path)
+	torch.save(model.state_dict(), save_model_path)
+
+
+print("=" * 60)
+print("finished training!")
+print("=" * 60)
+end_time = datetime.now().replace(microsecond=0)
+time_elapsed = str(end_time - start_time)
+end_time_str = end_time.strftime("%y-%m-%d-%H-%M-%S")
+print("started training at: " + start_time_str)
+print("finished training at: " + end_time_str)
+print("total training time: " + time_elapsed)
+print("max d4rl score: " + format(max_d4rl_score, ".5f"))
+print("saved max d4rl score model at: " + save_best_model_path)
+print("saved last updated model at: " + save_model_path)
+print("=" * 60)
+
+csv_writer.close()
